e04e0aac3e32604032586a499bc4436112e27644
Fikrat Teymur <teymurov.fikrat@gmail.com>
2023-02-07 11:20:42 +0400

    new one

--- script.js
+++ script.js
@@ -96,7 +96,7 @@ document.querySelector('.again').addEventListener('click', function () {
 });
 
 /* 
-Implement a game rest functionality, so that the player can make a new guess! Here is how:
+Implement a game rest functionality, so that the player can make a 
 
 1. Select the element with the 'again' class and attach a click event handler
 2. In the handler function, restore initial values of the score and secretNumber variables
